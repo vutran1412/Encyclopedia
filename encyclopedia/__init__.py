f59470728f5c8e3cf08c19e8ab1c3f6405c63df5
vutran <vutran1412@icloud.com>
2018-10-09 11:13:47 -0500

    Routing and Page layout completed

---
+++ encyclopedia/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '3ad2c1998384bfa7718e7f4799cbecb554b26578a99a4b06141fe48e3425be23e457c13963c33ee3c892ed4e5cc2228a07d97805d11a7238654725913b896a1f962d049fad60bc7b306110c8c10e3f210b742cd7476f27ea81bc0ddce2926b0c7b1328c4ab1c4f62b5d0e32ea4dd64ff98e251fa417e330a0ac15bdeb5a83191120ce6427ab5ac9e7d46cb0b99cb9ba7b01939b7187b6f889462576f9307d52dce1690a8f82d041369a32f3b96345224af3dfec0e4490f5ca53a5e2c055aa12727950680277ed220a2a2565252bd3a9034756df6fc4f698547bee58a6cf45080a78d707193537881817686a4760a445e31dc56d9568896d0f8e47329787bae67'
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from encyclopedia import routes
---
+++ encyclopedia/routes.py
@@ -0,0 +1,76 @@
+from encyclopedia import app, db, bcrypt
+from flask import render_template, url_for, flash, redirect, request
+from encyclopedia.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from encyclopedia.models import User, Source
+from flask_login import login_user, logout_user, current_user, login_required
+
+sources = [
+    {
+        'title': 'Crocodiles',
+        'content': 'Nature\'s deadliest predator',
+        'date_accessed': 'October 10, 2018',
+        'url': 'https://en.wikipedia.org/wiki/Crocodile'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', title='Encyclopedia Researcher', sources=sources)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='about')
+
+
+@app.route('/search')
+def search():
+    return render_template('search.html', title='search')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please Check the email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route("/account")
+@login_required
+def account():
+    form = UpdateAccountForm()
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account',
+                           image_file=image_file, form=form)
